5276e460e40390e450545d7fbdd40de45d27cb39
llxdaxia <18983679028@163.com>
2016-02-28 21:49:41 +0800

    网络学习

--- app/src/main/java/cn/hotwoo/alien/servicelife/model/server/SchedulerTransformer.java
+++ app/src/main/java/cn/hotwoo/alien/servicelife/model/server/SchedulerTransformer.java
@@ -12,7 +12,7 @@ import rx.schedulers.Schedulers;
 public class SchedulerTransformer<T> implements Observable.Transformer<T, T> {
     @Override
     public Observable<T> call(Observable<T> tObservable) {
-        return tObservable
+        return tObservable     //----这里这能这样写-----
                 .doOnError(new Action1<Throwable>() {
                     @Override
                     public void call(Throwable throwable) {
@@ -22,5 +22,18 @@ public class SchedulerTransformer<T> implements Observable.Transformer<T, T> {
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .unsubscribeOn(Schedulers.io());
+
+        //----不能这样写，以为这里是异步操作，当网络还在请求数据的时候tObservable就返回了，会抛出NetworkOnMainThreadException异常
+//        tObservable
+//                .doOnError(new Action1<Throwable>() {
+//                    @Override
+//                    public void call(Throwable throwable) {
+//                        Utils.Log(throwable.getLocalizedMessage());
+//                    }
+//                })
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .unsubscribeOn(Schedulers.io());
+//        return tObservable;
     }
 }
